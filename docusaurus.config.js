a9d922058f31ec9ee9f9e709ea5ec9d8822c3b10
Vinit Tomar <vinit.tomar68@gmail.com>
2026-07-09 00:01:04 +0530

    base url updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
 
   // Update these two when you push to GitHub Pages:
   url: "https://your-github-username.github.io",
-  baseUrl: "/crack_interview/",
+  baseUrl: "/",
 
   organizationName: "your-github-username",
   projectName: "Crack Interview",
